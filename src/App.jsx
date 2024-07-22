2132c816b494789e8dbcbe23628002890132009e
Jeremiah Ope <opejeremiah@gmail.com>
2024-07-22 18:50:01 +0100

    WindTheta

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import {Chatbot} from "./Chatbot.jsx";
+
+function App() {
+
+    return (
+        <>
+            {/* <Header/> */}
+
+            <Chatbot/>
+
+            {/* <Footer/> */}
+        </>
+    )
+}
+
+export default App
+
+
+const Header = () => {
+    return (<>
+            <h1>my math teacher</h1>
+            <h2>a chatbot example</h2>
+        </>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            <a href="https://www.thetaedgecloud.com" target="_blank">
+                <img src={"https://www.thetaedgecloud.com/images/edgecloud-logo.svg"} className="logo"
+                     alt="Theta Edge Cloud logo"/>
+            </a>
+        </div>
+    )
+}
